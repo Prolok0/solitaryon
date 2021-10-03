4b10180a4fadcc9f2dd2fdcae77aded99b191edc
rcovery <ryanczpereira@gmail.com>
2021-10-03 20:44:37 -0300

    fix: index page

--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-const Home;
-
-export default Home
--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -4,7 +4,10 @@ class Home extends React.Component<any> {
     render() {
         return (
             <>
+            Hi
             </>
         );
     }
 }
+
+export default Home;
